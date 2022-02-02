bb9db56e7ec14775fde564fbb53ca6fb4b6c5860
Sistax <sami.tuori@gmail.com>
2022-02-02 13:19:34 +0200

    Erilliset viikko- ja vuosisarakkeet

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
 #Tässä haetaan data ja muokataan se PowerBI:n käyttöön.
-#Pyritään pärjäämään pienellä määrällä paketteja (base R:llä).
+#Pyritään pärjäämään suht pienellä määrällä paketteja (base R:llä).
 
 #pakettilataukset
 library(tidyr)
 library(dplyr)
+library(stringr)                         
 
 #Haetaan THL:n tartuntatautirekisteristä koronatapausten, testausten ja kuolemantapausten viikkotasoinen data.
 thl_api <- "https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.csv?row=dateweek20200101-509030&column=measure-444833.445356.492118.&&fo=1"
@@ -24,7 +25,11 @@ thldata <- csv_raw |> #Natiivi pipe-operaattori, vrt. tidyverse-paketin %>%
          Kuolemat = 'Kuolemantapausten lukumäärä') |>
   
   #Korvataan puuttuvat arvot nollilla
-  tidyr::replace_na(list(Testaukset = 0, Kuolemat = 0))
-
-saveRDS(thldata, "./data/thldata.RDS")
+  tidyr::replace_na(list(Testaukset = 0, Kuolemat = 0)) |>
+  
+  #Jaetaan Aika-sarake viikkosarakkeeseen ja vuosisarakkeeseen
+  dplyr::mutate(Viikko = str_sub(Aika,-2,-1),
+                Vuosi = str_sub(Aika,7, 10)) 
+  
+  
   
